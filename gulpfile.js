e10ed8eb498e97811d4b583856ff67560f464ad8
 Sofia <sc.comsa@mail.ru>
2019-12-07 20:00:48 +0500

    add: instagram button

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ function bs() {
     }
   });
   watch("./.html").on("change", browserSync.reload);
-  watch("./sass/**/.sass", serveSass);
-  watch("./sass/**/.scss", serveSass);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/.js").on("change", browserSync.reload);
 }
 
