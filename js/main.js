2f49bcaaca1ed12ade68238f6a5046f41d4823cc
 Sofia <sc.comsa@mail.ru>
2020-01-14 16:06:57 +0500

    feat: form validation

--- js/main.js
+++ js/main.js
@@ -80,6 +80,71 @@ $(document).ready(function () {
 
   });
 
+  $('.control__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      },
+    }, //сообщения
+    messages: {
+      userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв"
+      }, 
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите в формате: name@domain.com"
+      }
+    }
+
+  });
+
+  $('.footer__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // правило-объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      },
+      userQuestion: {
+        required: true,
+        minlength: 2
+      },
+    }, //сообщения
+    messages: {
+      userName: {
+      required: "Имя обязательно",
+      minlength: "Имя не короче двух букв"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите в формате: name@domain.com"
+      },
+      userQuestion: {
+        required: "Напишите Ваш вопрос",
+        minlength: "Вопрос не короче двух букв"
+        },
+    }
+
+  });
   // маска для телефона
 
   $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
